7dac60d5bb2fea495e027ea346956e8566193ac9
Pablo Saunite de Souza <pablosaunite@gmail.com>
2020-09-28 16:52:53 -0300

    Created entry point and conversor component

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <div class="container pt-3">
+      <h1>How Much Is</h1>
+      <h3>Your better online currency conversor</h3>
+    </div>
+    <div id="dashboard" class="container">
+      <div class="d-flex row justify-content-center align-items-center">
+        <Conversor />
+      </div>
+    </div>
+    <div class="card mt-2 shadow bg-white rounded fixed-bottom">
+      <div class="card-body">
+        <h5 class="card-title">How Much is</h5>
+        <p class="card-text">
+          Donate: my Acounts...
+        </p>
+        <p class="card-text">
+          <small class="text-muted">Copyright 2020 - ©howmuchis<br>Powered by Pablo Saunite de Souza.</small>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import Conversor from "./components/Conversor.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Conversor
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #ddd;
+  width: 100vw;
+  height: 100vh;
+}
+
+.container {
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+
+</style>
---
+++ src/components/Conversor.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="conversor" class="card shadow p-1 m-3 bg-white rounded">
+    <div class="card-body">
+      <h5 class="card-title">Conversor</h5>
+      <form class="d-flex row align-items-center justify-content-center" autocomplete="off"
+      v-on:submit.prevent="">
+        <div class="form-group m-2">
+          <select id="selectCurrency0" class="form-control m-1">
+            <option selected >BRL</option>
+          </select>
+          <input type="number" class="form-control m-1" id="inputCurrency0" >
+        </div>
+        <div class="form-group m-2">
+          <select id="selectCurrency1" class="form-control m-1">
+            <option selected >USD</option>
+          </select>
+          <input disabled class="form-control m-1" id="inputCurrency1" >
+        </div>
+        <button class="btn btn-primary btn-block" type="submit">Convert</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'conversor'
+}
+</script>
+
+<style scoped>
+
+#conversor {
+  max-width: 500px;
+}
+
+.form-control {
+  max-width: 200px;
+}
+
+</style>
